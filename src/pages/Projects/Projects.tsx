c9cfede7b15eccec567e1c72acc781a5fd625a00
Brooke Yombalakian <penciltea@aol.com>
2025-09-05 11:32:33 -0400

    adding new project to portfolio

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -3,22 +3,29 @@ import arahayes from '../../assets/images/aranaiahayes.jpg'
 import handfulwhimsy from '../../assets/images/handfulwhimsy.jpg'
 import dsweb from '../../assets/images/dsweb.jpg'
 import vat from '../../assets/images/vat.jpg'
+import realmfoundry from '../../assets/images/realmfoundry.png'
 
 import './Projects.scss';
 
 function ProjectsComponent(){
     const projects = [
         {
-            title: "dsweb.safelink.com",
-            link: "https://dsweb.safelink.com/611611/help",
-            image: dsweb,
-            description: "This is a fully responsive site designed to allow the client's users to get the most out of their mobile devices. It features various REST API integrations, including dynamic header and footer content and hooks to send SMS messages to US phone numbers, allowing users to send themselves SMS messages to their phone, regardless of what device they're accessing the site from. "
+            title: "RealmFoundry",
+            link: "https://realmfoundry.app",
+            image: realmfoundry,
+            description: "This is a personal project that I am currently developing. It is a web application designed to help TTRPG GMs, world-builders, and storytellers create and manage their worlds, locations, NPCs, and more. Users can enter data for a variety of forms of content or use rules-based logic to generate data instead. It features a responsive design for use across multiple device types, user authentication with NextAuth, a robust UI with Next.js, MUI, RHF, and Zod, and a MongoDB back-end with Mongoose."
         },
         {
             title: "virtualairtimecard.com",
             link: "https://virtualairtimecard.com?rtl=tgt",
             image: vat,
             description: "This mobile-oriented site was a pandemic project. The client wanted to allow their users to purchase minute service plans for their phones from retail locations without the need to obtain a physical plan card, allowing users to go directly to the cashier to pay for the product. The website features various REST API integrations, including rendering barcodes to verifying the purchase was successful, and renders various happy and unhappy paths throughout the flow. Users can also get a SMS message to activate the plan directly onto their phone."
+        },        
+        {
+            title: "dsweb.safelink.com",
+            link: "https://dsweb.safelink.com/611611/help",
+            image: dsweb,
+            description: "This is a fully responsive site designed to allow the client's users to get the most out of their mobile devices. It features various REST API integrations, including dynamic header and footer content and hooks to send SMS messages to US phone numbers, allowing users to send themselves SMS messages to their phone, regardless of what device they're accessing the site from. "
         },
         { 
             title: "aranaiahayes.carrd.co",
